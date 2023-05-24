6d7b55b0a4a2e5113be49907310052615d5bbf88
JoseM <pcasjosemiguel@gmail.com>
2023-05-24 13:43:01 -0600

    Cambios en las carpetas

---
+++ script.js
@@ -0,0 +1,56 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+//btnEncriptar() ejecuta el encriptado
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+//encriptar(stringEncriptada) crea el encriptado a ejecutar
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+
+        }
+
+    }
+    return stringEncriptada;
+}
+
+//btnDesencriptar() ejecuta el metodo para desencritar
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    
+}
+
+//desencriptar(stringDesencriptada)  metodo para desencritar
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+
+        }
+
+    }
+    return stringDesencriptada;
+}
+
+//copia el mensaje para desencriptarlo o reencriptarlo
+function btnCopiar(){
+    const copiar = mensaje.value;
+    mensaje.value = "";
+    textArea.value = copiar;
+}
+
